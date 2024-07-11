b30e5636b25ffcb2f7f2317e8e7898589ac89e94
PhiNM <phinm@tohsoft.com>
2024-07-11 18:22:51 +0700

    Cập nhật demo, update target 34.

--- app/src/main/java/me/pqpo/smartcropper/MainActivity.java
+++ app/src/main/java/me/pqpo/smartcropper/MainActivity.java
@@ -3,18 +3,19 @@ package me.pqpo.smartcropper;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import java.io.File;
 
 public class MainActivity extends AppCompatActivity {
 
     Button btnTake;
-    Button btnSelect;
     ImageView ivShow;
 
     File photoFile;
@@ -23,25 +24,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        btnTake = (Button) findViewById(R.id.btn_take);
-        btnSelect = (Button) findViewById(R.id.btn_select);
-        ivShow = (ImageView) findViewById(R.id.iv_show);
+        btnTake = findViewById(R.id.btn_take);
+        ivShow = findViewById(R.id.iv_show);
+        photoFile = new File(getExternalFilesDir(""), "cropped.jpg");
 
-        photoFile = new File(getExternalFilesDir("img"), "scan.jpg");
+        if (photoFile.exists()) {
+            Bitmap bitmap = BitmapFactory.decodeFile(photoFile.getPath());
+            ivShow.setImageBitmap(bitmap);
+        }
 
-        btnTake.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivityForResult(CropActivity.getJumpIntent(MainActivity.this, false, photoFile), 100);
-            }
-        });
-
-        btnSelect.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivityForResult(CropActivity.getJumpIntent(MainActivity.this, true, photoFile), 100);
-            }
-        });
+        btnTake.setOnClickListener(v ->
+                startActivityForResult(
+                        CropActivity.getJumpIntent(MainActivity.this, photoFile),
+                        100
+                )
+        );
     }
 
     @Override
